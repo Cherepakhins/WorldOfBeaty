7cd47057f9fd0eab0155734eba70d415c48126a7
Cherepakhins <antoncerrek@gmail.com>
2023-05-28 16:16:54 +0700

    fixed video and tabs

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,14 +2,17 @@ const Modal = () => {
   const openPopUp = document.getElementById("open-pop-up");
   const closePopUp = document.getElementById("pop-up__close");
   const popUp = document.getElementById("pop-up");
+  const utVideo = document.querySelector('.ut-video')
 
   openPopUp.addEventListener("click", function (e) {
     e.preventDefault();
     popUp.classList.add("active");
+    utVideo.classList.remove('none')
   });
 
   closePopUp.addEventListener("click", () => {
     popUp.classList.remove("active");
+    utVideo.classList.add('none')
   });
 };
 
